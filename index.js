7cfc3fc2064cea64d84cd5fff232ee64ce9b4685
Gary <gary909@gmail.com>
2020-06-26 15:11:37 +0200

    JS

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ window.onload = function () {
     button.onclick = previewHandler;
 }
 
-window.onload = function () {
+function previewHandler() {
     var canvas = document.getElementById("tshirtCanvas");
     var context = canvas.getContext("2d");
-    context.fillRect(10, 10, 100, 100);
+
+    var selectObj = document.getElementById("shape");
+    var index = selectObj.selectedIndex;
+    var shape = selectObj[index].value
+
+    if (shape == "squares") {
+        for (var squares = 0; squares < 20; squares++) {
+            drawSquare(canvas, context);
+        }
+    }
 }
-
-
-var selectObj = document.getElementById("backgroundColor");
-var index = selectObj.selectedIndex;
-var bgColor = selectObj[index].value;
-
-var selectObj = document.getElementById("shape");
-var index = selectObj.selectedIndex;
-var shape = selectObj[index].value;
-
-var selectObj = document.getElementById("foregroundColor");
-var index = selectObj.selectedIndex;
-var fgColor = selectObj[index].value;
